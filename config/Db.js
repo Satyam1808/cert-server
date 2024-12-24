e86d67d697a565b3afc0a2b65c6c2da8ce469e01
satyam kumar <kmsatyam2019@gmail.com>
2024-12-24 20:23:25 +0530

    change_db.js

--- config/Db.js
+++ config/Db.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
       useUnifiedTopology: true,
       serverSelectionTimeoutMS: 30000,
     });
