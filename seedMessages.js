8bd7197351c4041473f6bb6f98cc5a6b233290e0
satyam kumar <kmsatyam2019@gmail.com>
2024-12-24 16:01:27 +0530

    database_url_added

--- seedMessages.js
+++ seedMessages.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const ChatbotMessage = require("./models/botModel");
 
 mongoose
-    .connect("mongodb://localhost:27017/CERT-In", { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("MongoDB connected"))
     .catch((error) => console.error("MongoDB connection failed:", error));
 
