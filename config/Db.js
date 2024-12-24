c9ef52efc0d50a0171b6410c40905f767ee4f3d7
satyam kumar <kmsatyam2019@gmail.com>
2024-12-24 17:02:55 +0530

    db_url_changed

--- config/Db.js
+++ config/Db.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000,
     });
     console.log('MongoDB connected...');
   } catch (err) {
--- seedMessages.js
+++ seedMessages.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const ChatbotMessage = require("./models/botModel");
+require('dotenv').config();
 
 mongoose
     .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
