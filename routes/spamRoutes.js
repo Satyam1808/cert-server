ebef80d9109d599d8683fef61071a42438c070d3
satyam kumar <kmsatyam2019@gmail.com>
2024-12-26 00:10:38 +0530

    changesInRoutes

--- routes/spamRoutes.js
+++ routes/spamRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { predictMessage, submitFeedback } = require('../controller/spamController');
 const router = express.Router();
-const userAuthMiddleware = require('../middleware/userAuthMiddleware');
+const userAuthMiddleware = require('../middlewares/userAuthMiddleware');
 
 router.post('/predict',userAuthMiddleware, predictMessage);
 router.post('/feedback', userAuthMiddleware, submitFeedback);
